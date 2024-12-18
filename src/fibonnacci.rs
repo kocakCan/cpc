3a29d7bb90a0959158222bbf8c435b2f9d56edeb
cankocak <can_kocak1@epam.com>
2024-12-18 21:35:09 +0300

    fixed the issue

--- src/fibonnacci.rs
+++ src/fibonnacci.rs
@@ -1,17 +1,18 @@
 fn main() {
-    println!("Please enter which Fibonnacci number you want to find");
-    let mut input = String::new();
-
-    std::io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
-
-    let input: u32 = input.trim().parse().expect("Please enter a number");
-
-    println!("The Fibonnacci number you are looking for is: {0}", fibonnacci(input));
+    // println!("Please enter which Fibonnacci number you want to find");
+    // let mut input = String::new();
+    //
+    // std::io::stdin()
+    //     .read_line(&mut input)
+    //     .expect("Failed to read line");
+    //
+    // let input: u64 = input.trim().parse().expect("Please enter a number");
+    //
+    // println!("The Fibonnacci number you are looking for is: {0}", fibonnacci(input));
+    println!("The Fibonnacci number you are looking for is: {}", fibonnacci(90));
 }
 
-fn fibonnacci(n: u32) -> u32 {
+fn fibonnacci(n: u64) -> u64 {
     if n == 0 || n == 1 {
         1
     } else {
@@ -20,7 +21,7 @@ fn fibonnacci(n: u32) -> u32 {
         let mut current = 1;
 
         // 1, 1, 2, 3, 5, 8, 13
-        while counter <= n {
+        while counter < n {
             current += prior;
             prior = current - prior;
             counter += 1;
